32d877f8ee0579a5903e09e9dd7e7e9d155398ac
miura-Miura <smiuymiu0official0@gmail.com>
2024-05-04 20:05:57 +0900

    add: 訓練時のloss,accuracyの格納クラスを定義

--- utils/eval_util.py
+++ utils/eval_util.py
@@ -5,6 +5,9 @@
 # Contact: github@malongtech.com
 #
 # This source code is licensed under the LICENSE file in the root directory of this source tree.
+import csv
+import dataclasses
+import matplotlib.pyplot as plt
 import torch
 
 
@@ -45,3 +48,47 @@ class AverageMeter():
             return str(self.val)
         
         return f'{self.val:.4f} ({self.avg:.4f})'
+
+@dataclasses.dataclass
+class RecordDataclass:
+  train_loss = ["train loss"]
+  valid_loss = ["valid loss"]
+  train_acc = ["train acc"]
+  valid_acc = ["valid acc"]
+
+  def add(self, train_loss, valid_loss, train_acc, valid_acc):
+    self.train_loss += [train_loss]
+    self.valid_loss += [valid_loss]
+    self.train_acc += [train_acc]
+    self.valid_acc += [valid_acc]
+
+
+  def save(self, path):
+    """ 各リストをCSVで保存する """
+    with open(path+'/history.csv', 'w', newline='') as file:
+      writer = csv.writer(file)
+      writer.writerow(self.train_loss)
+      writer.writerow(self.valid_loss)
+      writer.writerow(self.train_acc)
+      writer.writerow(self.valid_acc)
+
+    self._plot(path)
+    
+  def _plot(self, path):
+    fig1, ax1 = plt.subplots()
+    ax1.plot(self.train_loss[1:], label="training")
+    ax1.plot(self.valid_loss[1:], label="validation")
+    ax1.set_title("loss")
+    ax1.legend()
+    fig1.savefig(path + "/history_loss.png")
+
+    fig2, ax2 = plt.subplots()
+    ax2.plot(self.train_acc[1:], label="training")
+    ax2.plot(self.valid_acc[1:], label="validation")
+    ax2.set_title("accuracy")
+    ax2.legend()
+    fig2.savefig(path + "/history_accuracy.png")
+
+
+  def _len(self):
+    return  len(self.train_loss)
