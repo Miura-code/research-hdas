0874e2f2c87354d0ad63e186b74b3f8ca476aec0
miura-Miura <smiuymiu0official0@gmail.com>
2024-05-06 21:48:32 +0900

    experience: 実験の追加

--- run.sh
+++ run.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
+name=$1
+
+stage_architecture=("HS_DAS_CIFAR" "HS_DAS_CIFAR_SKIP" "STAGE_HSDAS_V1" "STAGE_HSDAS_V2" "STAGE_HSDAS_V3" "STAGE_SHALLOW" "STAGE_MIDDLE" "STAGE_DEEP" "STAGE_DARTS" )
 
 batch_size=128
 epoch=100
 train_portion=0.5
 seed=0
 
-python augmentStage_main.py \
-    --name finetuning_test  \
+# python augmentStage_main.py \
+#     --name $name  \
+#     --batch_size $batch_size \
+#     --dataset cifar10 \
+#     --epochs $epoch \
+#     --genotype DARTS_V1 \
+#     --DAG HS_DAS_CIFAR \
+#     --train_portion $train_portion \
+#     --seed $seed \
+
+for arch in "${stage_architecture[@]}"; do
+    echo $arch
+    python augmentStage_main.py \
+    --name $arch  \
     --batch_size $batch_size \
     --dataset cifar10 \
     --epochs $epoch \
     --genotype DARTS_V1 \
-    --DAG HS_DAS_CIFAR \
+    --DAG $arch \
     --train_portion $train_portion \
-    --seed $seed \
+    --seed $seed
+done
+
+# python searchStage_main.py \
+#     --name macro-cifar10-test \
+#     --w_weight_decay 0.0027  \
+#     --dataset cifar10 \
+#     --batch_size 64 \
+#     --workers 0  \
+#     --genotype DARTS_V1
