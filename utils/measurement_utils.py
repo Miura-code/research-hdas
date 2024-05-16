34ac1d2315bb2820f02dc57d773c24d8bb9bdf4d
miura-Miura <smiuymiu0official0@gmail.com>
2024-05-16 22:49:55 +0900

    fix: TimeKeeperの変数名を修正した

--- utils/measurement_utils.py
+++ utils/measurement_utils.py
@@ -5,12 +5,12 @@ from ptflops import get_model_complexity_info
 
 class TimeKeeper():
     def __init__(self):
-        self.start = time.time()
+        self.start_time = time.time()
     def end(self):
-        self.end = time.time()
-        self.time_diff = self.start - self.end
+        self.end_time = time.time()
+        self.time_diff = self.start - self.end_time
     def print_info(self):
-        return self.start, self.end, self.time_diff
+        return self.start_time, self.end_time, self.time_diff
 
 def count_ModelSize_byptflops(model, inputSize):
     # SUMMARY = summary(model, inputSize)
