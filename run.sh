c20bb06b2212558b62e15e67b2b3ca1365bd5faf
Miura-code <smiuymiu0official0@gamil.com>
2024-05-25 13:19:09 +0900

    add: 実験の追加

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 name=$1
 
-# stage_architecture=("HS_DAS_CIFAR" "HS_DAS_CIFAR_SKIP" "STAGE_HSDAS_V1" "STAGE_HSDAS_V2" "STAGE_HSDAS_V3" "STAGE_SHALLOW" "STAGE_MIDDLE" "STAGE_DEEP" "STAGE_DARTS" )
-stage_architecture=("STAGE_DARTS" "STAGE_DEEP" "STAGE_MIDDLE" "STAGE_SHALLOW")
+stage_architecture=("HS_DAS_CIFAR" "HS_DAS_CIFAR_SKIP" "STAGE_HSDAS_V1" "STAGE_HSDAS_V2" "STAGE_HSDAS_V3" "STAGE_SHALLOW" "STAGE_MIDDLE" "STAGE_DEEP" "STAGE_DARTS" )
+stage_architecture=("HS_DAS_CIFAR" "HS_DAS_CIFAR_SKIP" "STAGE_SHALLOW" "STAGE_MIDDLE" "STAGE_DEEP" "STAGE_DARTS" "STAGE_FULL_CASCADE")
 
 batch_size=128
-epoch=100
-train_portion=0.5
+epoch=600
+train_portion=1.0
 seed=0
 
 ## ステージの評価・ファインチューニング
@@ -21,21 +21,21 @@ seed=0
 #     --train_portion $train_portion \
 #     --seed $seed \
 
-# for seed in 1 2; do
-#     for arch in "${stage_architecture[@]}"; do
-#         echo $arch
-#         python augmentStage_main.py \
-#         --name $arch  \
-#         --batch_size $batch_size \
-#         --dataset cifar10 \
-#         --epochs $epoch \
-#         --genotype DARTS_V1 \
-#         --DAG $arch \
-#         --train_portion $train_portion \
-#         --seed $seed \
-#         --save eval
-#     done
-# done
+for seed in 1 2; do
+    for arch in "${stage_architecture[@]}"; do
+        echo $arch
+        python augmentStage_main.py \
+        --name $arch  \
+        --batch_size $batch_size \
+        --dataset cifar10 \
+        --epochs $epoch \
+        --genotype DARTS_V1 \
+        --DAG $arch \
+        --train_portion $train_portion \
+        --seed $seed \
+        --save E600
+    done
+done
 
 # for arch in "${stage_architecture[@]}"; do
 #     echo $arch
@@ -60,23 +60,24 @@ seed=0
 #     --genotype DARTS_V1
 
 ## ステージのテスト
-arch=$1
-seed=0
+# arch=$1
+# seed=0
 
-path=$2
-python testStage_main.py \
-    --name test \
-    --dataset cifar10 \
-    --batch_size 128 \
-    --genotype DARTS_V1 \
-    --DAG $arch \
-    --seed $seed \
-    --resume_path $path
+# path=$2
+# python testStage_main.py \
+#     --name test \
+#     --dataset cifar10 \
+#     --batch_size 128 \
+#     --genotype DARTS_V1 \
+#     --DAG $arch \
+#     --seed $seed \
+#     --resume_path $path
 
 # ディレクトリパスを指定
 # arch=$1
+# for arch in "${stage_architecture[@]}"; do
 # target_directory=/home/miura/lab/research-hdas/results/augment_Stage/cifar/$arch
-# specific_name="eval-"
+# specific_name="E600-"
 # seed=0
 # layer=20
 
@@ -100,4 +101,5 @@ python testStage_main.py \
 #             --resume_path $resume_path \
 #             --layer $layer
 #     fi
+# done
 # done
