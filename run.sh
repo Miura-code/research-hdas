89d64b551488927eb985b84c06da80a3841cfcf8
Miura-fire9 <smiuymiu0official0@gmail.com>
2024-05-28 20:53:17 +0900

    add: MNIST関連の実験を追加
    MNIST trained from SCRATCH
    CIFAR10 pretrained by MNIST

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 name=$1
 
-# stage_architecture=("HS_DAS_CIFAR" "HS_DAS_CIFAR_SKIP" "STAGE_HSDAS_V1" "STAGE_HSDAS_V2" "STAGE_HSDAS_V3" "STAGE_SHALLOW" "STAGE_MIDDLE" "STAGE_DEEP" "STAGE_DARTS" )
-stage_architecture=("STAGE_DARTS" "STAGE_DEEP" "STAGE_MIDDLE" "STAGE_SHALLOW")
+stage_architecture=("HS_DAS_CIFAR" "HS_DAS_CIFAR_SKIP" "STAGE_HSDAS_V1" "STAGE_HSDAS_V2" "STAGE_HSDAS_V3" "STAGE_SHALLOW" "STAGE_MIDDLE" "STAGE_DEEP" "STAGE_DARTS" )
+stage_architecture=("HS_DAS_CIFAR" "HS_DAS_CIFAR_SKIP" "STAGE_SHALLOW" "STAGE_MIDDLE" "STAGE_DEEP" "STAGE_DARTS" "STAGE_FULL_CASCADE")
 
 batch_size=64
 epoch=50
@@ -14,40 +14,37 @@ seed=0
 # python searchStage_main.py \
 #     --name $name \
 #     --batch_size $batch_size \
-#     --dataset mnist \
+#     --dataset CIFAR10 \
 #     --epochs $epoch \
 #     --genotype DARTS_V1 \
 #     --train_portion $train_portion \
-#     --seed $seed \
-#     --share_stage
+#     --seed $seed
 
 # for seed in 0 1 2 3; do
 #     echo $arch
 #     python searchStage_main.py \
 #         --name $name \
 #         --batch_size $batch_size \
-#         --dataset mnist \
+#         --dataset CIFAR10 \
 #         --epochs $epoch \
 #         --genotype DARTS_V1 \
 #         --train_portion $train_portion \
 #         --seed $seed \
-#         --share_stage \
-#         --gpus 1
 # done
 
 ## 事前学習アーキテクチャからステージの探索
-# checkpoint_path=$2
-# python searchStage_main.py \
-#     --name $name \
-#     --batch_size $batch_size \
-#     --dataset CIFAR10 \
-#     --epochs $epoch \
-#     --genotype DARTS_V1 \
-#     --train_portion $train_portion \
-#     --seed $seed \
-#     --share_stage \
-#     --resume_path $checkpoint_path \
-#     --checkpoint_reset
+checkpoint_path=$2
+python searchStage_main.py \
+    --name $name \
+    --batch_size $batch_size \
+    --dataset CIFAR10 \
+    --epochs $epoch \
+    --genotype DARTS_V1 \
+    --train_portion $train_portion \
+    --seed $seed \
+    --share_stage \
+    --resume_path $checkpoint_path \
+    --checkpoint_reset
 
 # checkpoints=(/home/miura/lab/research-hdas/results/search_Stage/mnist/SCRATCH/EXP-20240525-152620/best.pth.tar /home/miura/lab/research-hdas/results/search_Stage/mnist/SCRATCH/EXP-20240525-175220/best.pth.tar /home/miura/lab/research-hdas/results/search_Stage/mnist/SCRATCH/EXP-20240525-201945/best.pth.tar /home/miura/lab/research-hdas/results/search_Stage/mnist/SCRATCH/EXP-20240525-224521/best.pth.tar)
 # for checkpoint_path in "${checkpoints[@]}"; do
@@ -91,7 +88,7 @@ seed=0
 #         --DAG $arch \
 #         --train_portion $train_portion \
 #         --seed $seed \
-#         --save eval
+#         --save E600
 #     done
 # done
 
@@ -132,30 +129,42 @@ seed=0
 #     --resume_path $path
 
 # ディレクトリパスを指定
-arch=$1
-target_directory=/home/miura/lab/research-hdas/results/search_Stage/CIFAR10/SCRATCH
-specific_name="EXP-"
-seed=0
-layer=20
+# arch=$1
+# target_directory=/home/miura/lab/research-hdas/results/search_Stage/CIFAR10/SCRATCH
+# specific_name="EXP-"
+# seed=0
+# layer=20
 
-# # # ディレクトリ内の全てのディレクトリ名を取得し、test.pyを実行する
-for directory in "$target_directory"/*; do
-    # ディレクトリ名を変数に格納
-    directory_path="$directory"
-    if [[ "$directory_path" == *"$specific_name"* ]]; then
-        # ディレクトリ名を表示
-        echo "Directory name: $directory_path"
-        resume_path=$directory_path/best.pth.tar
-        echo $resume_path
+# # # # ディレクトリ内の全てのディレクトリ名を取得し、test.pyを実行する
+# for directory in "$target_directory"/*; do
+#     # ディレクトリ名を変数に格納
+#     directory_path="$directory"
+#     if [[ "$directory_path" == *"$specific_name"* ]]; then
+#         # ディレクトリ名を表示
+#         echo "Directory name: $directory_path"
+#         resume_path=$directory_path/best.pth.tar
+#         echo $resume_path
     
-        python testStage_main.py \
-            --name test \
-            --dataset cifar10 \
-            --batch_size 128 \
-            --genotype DARTS_V1 \
-            --DAG $arch \
-            --seed $seed \
-            --resume_path $resume_path \
-            --layer $layer
-    fi
-done
+#         python testStage_main.py \
+#             --name test \
+#             --dataset cifar10 \
+#             --batch_size 128 \
+#             --genotype DARTS_V1 \
+#             --DAG $arch \
+#             --seed $seed \
+#             --resume_path $resume_path \
+#             --layer $layer
+#     fi
+# done
+#         python testStage_main.py \
+#             --name test \
+#             --dataset cifar10 \
+#             --batch_size 128 \
+#             --genotype DARTS_V1 \
+#             --DAG $arch \
+#             --seed $seed \
+#             --resume_path $resume_path \
+#             --layer $layer
+#     fi
+# done
+# done
