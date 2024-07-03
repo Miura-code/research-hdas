6437ee3205f1ee98d7e587a0468c860f11592b45
miura-Miura <smiuymiu0official0@gmail.com>
2024-07-03 19:40:17 +0900

    add: BASELINEのセル構造を追加

--- genotypes/genotype_cell.py
+++ genotypes/genotype_cell.py
@@ -148,3 +148,77 @@ PDARTS = Genotype(
     ], 
     reduce_concat=range(2, 6)
 )
+
+BASELINE1 = Genotype3(
+    normal1=[
+        [('sep_conv_3x3', 1), ('sep_conv_3x3', 0)], 
+        [('sep_conv_3x3', 0), ('sep_conv_3x3', 2)], 
+        [('sep_conv_3x3', 2), ('sep_conv_3x3', 0)], 
+        [('sep_conv_3x3', 0), ('sep_conv_3x3', 4)]
+    ], 
+    normal1_concat=range(2, 6), 
+    reduce1=[
+        [('sep_conv_3x3', 0), ('sep_conv_3x3',1)], 
+        [('skip_connect', 1), ('avg_pool_3x3', 0)], 
+        [('sep_conv_3x3', 2), ('sep_conv_3x3', 1)], 
+        [('avg_pool_3x3', 0), ('skip_connect', 3)]
+    ], 
+    reduce1_concat=range(2, 6), 
+    normal2=[
+        [('skip_connect', 0), ('skip_connect', 1)], 
+        [('skip_connect', 1), ('skip_connect', 0)], 
+        [('skip_connect', 0), ('sep_conv_3x3', 2)], 
+        [('skip_connect', 0), ('skip_connect', 1)]
+    ], 
+    normal2_concat=range(2, 6), 
+    reduce2=[
+        [('avg_pool_3x3', 0), ('avg_pool_3x3', 1)], 
+        [('avg_pool_3x3', 0), ('avg_pool_3x3', 1)], 
+        [('avg_pool_3x3', 0), ('skip_connect', 2)], 
+        [('avg_pool_3x3', 0), ('skip_connect', 2)]
+    ], 
+    reduce2_concat=range(2, 6), 
+    normal3=[
+        [('skip_connect', 0), ('dil_conv_3x3', 1)], 
+        [('skip_connect', 0), ('skip_connect', 1)], 
+        [('skip_connect', 0), ('skip_connect', 1)], 
+        [('skip_connect', 0), ('skip_connect', 2)]
+    ], normal3_concat=range(2, 6))
+
+BASELINE2 = Genotype3(
+    normal1=[
+        [('dil_conv_5x5', 0), ('dil_conv_5x5', 1)], 
+        [('sep_conv_5x5', 0), ('dil_conv_3x3', 2)], 
+        [('sep_conv_3x3', 0), ('max_pool_3x3', 1)], 
+        [('sep_conv_5x5', 1), ('dil_conv_3x3', 4)]
+    ], 
+    normal1_concat=range(2, 6), 
+    reduce1=[
+        [('dil_conv_5x5', 1), ('dil_conv_3x3',0)], 
+        [('dil_conv_3x3', 1), ('max_pool_3x3', 0)], 
+        [('dil_conv_5x5', 1), ('dil_conv_5x5', 3)], 
+        [('dil_conv_5x5', 3), ('dil_conv_3x3', 2)]
+    ], 
+    reduce1_concat=range(2, 6), 
+    normal2=[
+        [('dil_conv_3x3', 1), ('sep_conv_5x5', 0)], 
+        [('sep_conv_3x3', 0), ('dil_conv_5x5', 1)], 
+        [('sep_conv_5x5', 0), ('avg_pool_3x3', 1)], 
+        [('sep_conv_3x3', 2), ('sep_conv_5x5', 1)]
+    ], 
+    normal2_concat=range(2, 6), 
+    reduce2=[
+        [('sep_conv_3x3', 0), ('dil_conv_5x5', 1)], 
+        [('sep_conv_3x3', 2), ('sep_conv_5x5', 1)], 
+        [('skip_connect', 0), ('sep_conv_5x5', 1)], 
+        [('sep_conv_3x3', 3), ('dil_conv_5x5', 1)]
+    ], 
+    reduce2_concat=range(2, 6), 
+    normal3=[
+        [('sep_conv_5x5', 0), ('dil_conv_5x5', 1)], 
+        [('sep_conv_3x3', 2), ('sep_conv_3x3', 0)], 
+        [('dil_conv_5x5', 2), ('sep_conv_3x3', 1)], 
+        [('sep_conv_5x5', 4), ('sep_conv_5x5', 2)]
+    ], 
+    normal3_concat=range(2, 6)
+) 
