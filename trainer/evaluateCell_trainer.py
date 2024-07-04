6454557760c569f71c30538128978ac7cf1aafce
miura-Miura <smiuymiu0official0@gmail.com>
2024-07-04 21:27:13 +0900

    experiment: セル構造のベースラインを評価

--- trainer/evaluateCell_trainer.py
+++ trainer/evaluateCell_trainer.py
@@ -66,11 +66,10 @@ class EvaluateCellTrainer():
 
         # datasetを任意の割合で分割する
         n_train = len(train_data)
-        split_half = n_train // 2
         split = int(np.floor(self.config.train_portion * n_train))
         indices = list(range(n_train))
         train_sampler = torch.utils.data.sampler.SubsetRandomSampler(indices[:split])
-        valid_sampler = torch.utils.data.sampler.SubsetRandomSampler(indices[split_half:split_half+split])
+        valid_sampler = torch.utils.data.sampler.SubsetRandomSampler(indices[split:])
         self.train_loader = torch.utils.data.DataLoader(train_data,
                                                         batch_size=self.config.batch_size,
                                                         sampler=train_sampler,
